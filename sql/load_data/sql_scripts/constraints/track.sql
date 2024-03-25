b5acd4dcfe7e50a04c714cc03b589515976094fc
Patrick Francke <36266641+Franckyboi@users.noreply.github.com>
2024-03-25 10:24:46 +0100

    remove bad constraint structure

--- sql/load_data/sql_scripts/constraints/track.sql
+++ sql/load_data/sql_scripts/constraints/track.sql
@@ -2,12 +2,7 @@ SET foreign_key_checks = 0;
 
 USE `DtoS`;
 
- -- Start error handler to continue execution on error
-DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
-
 ALTER TABLE `track` ADD CONSTRAINT PRIMARY KEY (`id`);
 ALTER TABLE `track` ADD CONSTRAINT `track_ibfk_1` FOREIGN KEY (`release_id`) REFERENCES `release` (`id`) ON DELETE RESTRICT;
 
--- Reset error handler
-DECLARE CONTINUE HANDLER FOR SQLEXCEPTION RESIGNAL;
 
