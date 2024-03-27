3d03ac62da6d6876b2fa7ab2e37e5507cdee494c
Patrick Francke <superchaschper@gmail.com>
2024-03-27 22:52:14 +0100

    fix artist load Query

--- sql/load_data/sql_scripts/loadData/artist.sql
+++ sql/load_data/sql_scripts/loadData/artist.sql
@@ -8,6 +8,6 @@ LOAD DATA INFILE '/var/lib/mysql-files/table_data/artist.csv'
     LINES TERMINATED BY '\r\n' 
     IGNORE 1 LINES
     (id, @name)
-    SET title = LEFT(@name, 768);
+    SET name = LEFT(@name, 768);
 
 ;
