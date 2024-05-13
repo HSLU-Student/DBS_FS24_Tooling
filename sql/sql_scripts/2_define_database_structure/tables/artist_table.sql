e9f0abbe4508d7b2a9efaf8c8972c4e619d0b284
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-05-13 23:04:46 +0200

    resolve artist issue

--- sql/sql_scripts/2_define_database_structure/tables/artist_table.sql
+++ sql/sql_scripts/2_define_database_structure/tables/artist_table.sql
@@ -8,9 +8,14 @@ USE `DtoS`;
 SET NAMES utf8mb4;
 
 CREATE TABLE `artist` (
+<<<<<<< HEAD:sql/sql_scripts/2_define_database_structure/tables/artist_table.sql
   `artist_id` int NOT NULL,
   `name` varchar(450) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `search_name` varchar(768) DEFAULT NULL
+=======
+  `id` int NOT NULL,
+  `name` varchar(768) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+>>>>>>> main:sql/load_data/sql_scripts/noFKConstraints/artist.sql
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
