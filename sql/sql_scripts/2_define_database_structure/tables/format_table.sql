77a60394b6c2ab44bff982509153463d1f2223f2
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-05-13 23:39:09 +0200

    fix format.sql

--- sql/sql_scripts/2_define_database_structure/tables/format_table.sql
+++ sql/sql_scripts/2_define_database_structure/tables/format_table.sql
@@ -7,16 +7,7 @@ USE `DtoS`;
 
 SET NAMES utf8mb4;
 
-<<<<<<< HEAD:sql/sql_scripts/2_define_database_structure/tables/format_table.sql
 CREATE TABLE `format` (
   `format_id` int NOT NULL,
   `format` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-=======
-CREATE TABLE `artist` (
-  `id` int NOT NULL,
-  `name` varchar(768) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
->>>>>>> main:sql/load_data/sql_scripts/noFKConstraints/artist.sql
