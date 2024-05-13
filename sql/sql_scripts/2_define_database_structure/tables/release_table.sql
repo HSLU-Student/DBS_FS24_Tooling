4c65bd118827726589fa64f0ba704b4adba465a4
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-05-13 23:37:34 +0200

    remove unneeded attribute

--- sql/sql_scripts/2_define_database_structure/tables/release_table.sql
+++ sql/sql_scripts/2_define_database_structure/tables/release_table.sql
@@ -11,6 +11,5 @@ CREATE TABLE `release` (
   `release_id` int NOT NULL,
   `title` varchar(768) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `released` year DEFAULT NULL,
-  `search_name` varchar(768) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
