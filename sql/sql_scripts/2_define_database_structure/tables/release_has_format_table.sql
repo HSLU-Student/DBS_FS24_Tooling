eb8efee333d8378581b7e69bf68f45f52a65cc3c
Patrick Francke <superchaschper@gmail.com>
2024-05-18 17:25:36 +0200

    update load statements for schema changes

--- sql/sql_scripts/2_define_database_structure/tables/release_has_format_table.sql
+++ sql/sql_scripts/2_define_database_structure/tables/release_has_format_table.sql
@@ -10,5 +10,4 @@ SET NAMES utf8mb4;
 CREATE TABLE `release_has_format` (
   `release_id` int NOT NULL,
   `format_id` int  NOT NULL,
-  `format` varchar(100) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- sql/sql_scripts/2_define_database_structure/tables/track_has_artist_table.sql
+++ sql/sql_scripts/2_define_database_structure/tables/track_has_artist_table.sql
@@ -9,7 +9,6 @@ SET NAMES utf8mb4;
 
 CREATE TABLE `track_has_artist` (
   `track_id` varchar(20) NOT NULL  ,
-  `artist_id` int NOT NULL  ,
-  `artist_name` varchar(768) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+  `artist_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
