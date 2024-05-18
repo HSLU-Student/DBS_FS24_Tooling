fa6d5bbb966420a68a6a467d67b25f1d18764f59
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-05-18 21:53:48 +0200

    adjust sql queries to new db schema

--- sql/sql_scripts/4_query_database/no_of_tracks_on_release.sql
+++ sql/sql_scripts/4_query_database/no_of_tracks_on_release.sql
@@ -1,10 +1,11 @@
-USE dtos;
-SELECT DISTINCT t.title AS `Track Title`,
-a.name AS `Artist Name`,
+SELECT DISTINCT
 r.title AS `Release Title`,
 f.format AS `Format`,
-r.released AS `Release Year`, CONCAT("discogs.com/sell/release/",r.release_id) AS `Buy On Dicogs` FROM format f
--- Join release_has_format & format
+r.released AS `Release Year`, 
+CONCAT("discogs.com/sell/release/",r.release_id) AS `Buy on Dicogs`,
+COUNT(*) AS `Tracks from playlist in release`
+FROM format f
+-- Join release_has_format & format 
 JOIN release_has_format rf ON f.format_id  = rf.format_id
 -- Join release & release_has_format
 JOIN `release` r ON rf.release_id = r.release_id
@@ -17,22 +18,22 @@ JOIN artist a ON ta.artist_id = a.artist_id
 
 -- Join spotify_track & track (lookup by search_name)
 JOIN spotify_track st ON t.search_name = st.search_name
--- Join spotify_artist & spotify_track (lookup by search_name)
-JOIN spotify_artist sa ON st.artist_id = sa.artist_id AND a.search_name = sa.search_name
+-- Join spotify_artist on spotify_track (lookup by search_name)
+JOIN spotify_artist sa ON st.artist_id = sa.artist_id and a.search_name = sa.search_name
 -- Join spotify_playlist_has_spotify_track & spotify_track
-JOIN spotify_playlist_has_spotify_track sphst ON st.track_id = sphst.track_id
--- Join spotify_playlist & spotify_playlist_has_spotify_track
+JOIN spotify_playlist_has_spotify_track sphst on st.track_id = sphst.track_id
+-- Join spotify_playlist & spotify_playlist_has_spotify_track 
 JOIN spotify_playlist sp ON sphst.playlist_id = sp.playlist_id
 -- Join spotify_user & spotify_playlist
 JOIN spotify_user su ON sp.user_id = su.user_id
 
--- Filter User & Playlist
+-- Filter & Playlist
 WHERE su.user_name = 'TheKetio' AND sp.name = 'diesdas'
--- Filter Track
-AND t.title = 'House of the Rising Sun'
--- Filter Artist
-AND a.name = 'The Animals'
 -- Filter Format
-AND f.format = 'CD' 
+AND f.format = 'Vinyl' 
 -- Filter Release year
-AND r.released >= '1999'
+AND r.released >= '1901'
+-- Group releases with multiple tracks
+GROUP BY r.title, f.format, r.released,CONCAT("discogs.com/sell/release/",r.release_id)
+-- Releaes with most tracks from playlist, show ontop
+ORDER BY COUNT(*) DESC 
