2b6c62586484d18ad193a1eccb4289e52f29dfe9
Patrick Francke <superchaschper@gmail.com>
2024-05-18 17:41:26 +0200

    update released type in table definition

--- sql/sql_scripts/2_define_database_structure/tables/release_table.sql
+++ sql/sql_scripts/2_define_database_structure/tables/release_table.sql
@@ -10,6 +10,6 @@ SET NAMES utf8mb4;
 CREATE TABLE `release` (
   `release_id` int NOT NULL,
   `title` varchar(768) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `released` year DEFAULT NULL,
+  `released` SMALLINT DEFAULT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
