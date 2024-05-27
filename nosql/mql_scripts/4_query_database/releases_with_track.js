909371d0dd7041acc6e69c822147e0eeb4d3132f
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-05-27 23:43:51 +0200

    add basic release lookup query

---
+++ nosql/mql_scripts/4_query_database/releases_with_track.js
@@ -0,0 +1,44 @@
+use('dtos')
+
+db.spotify_users2.aggregate([
+    //find the user in the spotify DB
+    {$match: {"spotify_user.username": "TheKetio"}},
+
+    //unwind all user playlists
+    {$unwind: "$spotify_user.playlists"},
+
+    //match only the selected playlist
+    {$match: {"spotify_user.playlists.name": "diesdas"}},
+
+    //unwind all playlist tracks
+    {$unwind: "$spotify_user.playlists.playlist_tracks"},
+
+    //set the playlist tracks as new root to flatten the structure
+    {$replaceRoot: {newRoot: "$spotify_user.playlists.playlist_tracks"}},
+
+    //lookup possible artists foreach track. If we can't find any objects here, this means the requested artist is not stored in our database.
+    {$lookup: {
+          from: "artists2",
+          localField: "artist_search_name",
+          foreignField: "artist.search_name",
+          as: "matched_artists"
+        }
+    },
+
+    //once again unwinding the remaining possible artists
+    {$unwind: "$matched_artists"},
+    
+    //remove all tracks which do not match the track_search_name
+    {$set: {
+            "matched_artists.artist.tracks": {
+                $filter: {
+                    input: "$matched_artists.artist.tracks",
+                    cond: { $eq: ["$$this.search_name", "$track_search_name"]}
+                } 
+            }
+        }
+    },
+    
+    //TODO restructuring & filtering some stuff -> project the output
+
+])
