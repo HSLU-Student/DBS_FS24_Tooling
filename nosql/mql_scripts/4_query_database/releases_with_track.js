1a8ea0bca3ad42629e4cffb33a56b4b1110bd5d3
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-05-28 08:19:56 +0200

    finish release_with_tracks mql query

--- nosql/mql_scripts/4_query_database/releases_with_track.js
+++ nosql/mql_scripts/4_query_database/releases_with_track.js
@@ -2,22 +2,25 @@ use('dtos')
 
 db.spotify_users2.aggregate([
     //find the user in the spotify DB
-    {$match: {"spotify_user.username": "TheKetio"}},
+    { $match: { "spotify_user.username": "TheKetio" } },
 
     //unwind all user playlists
     {$unwind: "$spotify_user.playlists"},
 
     //match only the selected playlist
-    {$match: {"spotify_user.playlists.name": "diesdas"}},
+    { $match: { "spotify_user.playlists.name": "diesdas" } },
 
     //unwind all playlist tracks
-    {$unwind: "$spotify_user.playlists.playlist_tracks"},
+    { $unwind: "$spotify_user.playlists.playlist_tracks" },
 
+    //optional! filter by track by removing all documents not having the selected track name 
+    { $match: { "spotify_user.playlists.playlist_tracks.track_title": "House of the Rising Sun" } },
+    
     //set the playlist tracks as new root to flatten the structure
-    {$replaceRoot: {newRoot: "$spotify_user.playlists.playlist_tracks"}},
+    { $replaceRoot: { newRoot: "$spotify_user.playlists.playlist_tracks" } },
 
     //lookup possible artists foreach track. If we can't find any objects here, this means the requested artist is not stored in our database.
-    {$lookup: {
+    { $lookup: {
           from: "artists2",
           localField: "artist_search_name",
           foreignField: "artist.search_name",
@@ -26,10 +29,10 @@ db.spotify_users2.aggregate([
     },
 
     //once again unwinding the remaining possible artists
-    {$unwind: "$matched_artists"},
+    { $unwind: "$matched_artists" },
     
     //remove all tracks which do not match the track_search_name
-    {$set: {
+    { $set: {
             "matched_artists.artist.tracks": {
                 $filter: {
                     input: "$matched_artists.artist.tracks",
@@ -39,6 +42,25 @@ db.spotify_users2.aggregate([
         }
     },
     
-    //TODO restructuring & filtering some stuff -> project the output
+    //one last unwinding into all release documents
+    { $unwind: "$matched_artists.artist.tracks" },
 
+    //restructure in clean document, we can filter on & project the wished fields from
+    {
+        $replaceWith: {
+            release: {
+                "title": "$track_title",
+                "artist": "$artist_name",
+                "release": "$matched_artists.artist.tracks.release" 
+            } 
+        }
+    },
+    
+    //optional! filtering on year
+    { $match: { "release.release.released": {$gte: 1999}}},
+
+    //optional filtering on format
+    { $match: { "release.release.formats": { $in: ["CD"] } } }
+    
+    //will add the project stage, as needed in metabase
 ])
