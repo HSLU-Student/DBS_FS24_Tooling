eb51182084c953483398a6aca352f2d3dfcd1233
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-06-01 11:44:25 +0200

    remove comment

--- nosql/mql_scripts/4_query_database/no_of_tracks.js
+++ nosql/mql_scripts/4_query_database/no_of_tracks.js
@@ -3,7 +3,6 @@ use('dtos')
 db.artists.aggregate([
     { $unwind: "$artist.tracks" },
 
-    //technically we don't need this grouping as one track = one release
     { $group: {
           _id: "$artist.tracks.track_id"
         }
