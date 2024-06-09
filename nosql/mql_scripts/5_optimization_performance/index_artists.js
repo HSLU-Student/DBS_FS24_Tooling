e86c9ea6d7d29ae00f700658d4073f6d7692aa36
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2024-06-09 20:49:25 +0200

    Remove no longer needed ToDo

--- nosql/mql_scripts/5_optimization_performance/index_artists.js
+++ nosql/mql_scripts/5_optimization_performance/index_artists.js
@@ -1,3 +1,3 @@
 use('dtos')
-//TODO test a compound index, to combine artist & tracks searchname for further speedup
-db.artists.createIndexes([{"artist.search_name":1},{"artist.tracks.search_name":1}])
+
+db.artists.createIndexes([{ "artist.search_name": 1 }, { "artist.tracks.search_name": 1 }])
